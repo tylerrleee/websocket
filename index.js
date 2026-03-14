9f9265d786c3d842ed17bdd216b074f771b89560
tylerrleee <160276402+tylerrleee@users.noreply.github.com>
2026-03-13 23:18:36 -0400

    emitting listening added

--- index.js
+++ index.js
@@ -1,79 +1,33 @@
-const express = require('express');
-const { createServer } = require('node:http');
-const { join } = require('node:path');
-const { Server } = require('socket.io');
-const sqlite3 = require('sqlite3');
-const { open } = require('sqlite');
+require('dotenv').config();
 
-async function main(){
-    // open database file
-    const db = await open({
-        filename: 'chat.db',
-        driver: sqlite3.Database
+var express = require('express');
+var socket = require('socket.io');
+var cors = require('cors')
+
+const PORT = process.env.PORT || 4000;
+
+
+// App setup
+var app = express();
+var server = app.listen(PORT, function(){
+    console.log(`listening for requests on http://localhost:${PORT}`);
+});
+
+// Static files
+app.use(cors())
+app.use(express.static('public'));
+
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+
+    socket.on('chat', function(data){
+        io.sockets.emit('chat', data);
     });
 
-    // create 'messages' table 
-    await db.exec(`
-        CREATE TABLE IF NOT EXISTS messages (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        client_offset TEXT UNIQUE,
-        content TEXT 
-        );
-    `);
-
-    const app = express();
-    const server = createServer(app);
-    const io = new Server(server, {
-        connectionStateRecovery: {}
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
     });
-
-    // Static files
-    app.get('/', (req, res) => {
-        res.sendFile(join(__dirname, './public/index.html'));
-    });
-
-    // Make new connection w/ new user
-    io.on('connection', async (socket) => {
-        socket.broadcast.emit('hi');
-        socket.on('chat message', async (msg, clientOffset, callback) => {
-            let result;
-            try {
-                // store message in db
-                result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
-            } catch (e) {
-                if (e.errno === 19 /*SQLITE_CONSTRAINT */){
-                    // msg already inserted, so notify client
-                    callback();
-                } else{
-                    // nothing, let client retry
-                }
-                // TODO Handle failure
-                return;
-            }
-            io.emit('chat message', msg, result.lastID);
-            // acknowledge event
-            callback();
-        });
-
-        if (!socket.recovered) {
-            // if connection state recovery not successful
-            try {
-                await db.each('SELECT id, content from messages WHERE id > ?',
-                    [socket.handshake.auth.serverOffset || 0],
-                    (_err, row) => {
-                        socket.emit('chat message', row.content, row.id);
-                    }
-                )
-            } catch (e) {
-                // something wrong
-            }
-        }
-    });
-
-    // function(){} is the same as () => {}
-    server.listen(4000, () => {
-        console.log('server runnign at http://localhost:4000');
-    });
-}
-
-main();
+});
